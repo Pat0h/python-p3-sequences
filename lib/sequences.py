0c164f3fc3fb3dfb4bdf08aebb6c359fc0755c98
Pat0h <patrickngatia14@gmail.com>
2023-11-26 10:57:42 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print('[]')
+        return []
+    elif length == 1:
+        print('[0]')
+        return [0]
+    elif length == 2:
+        print('[0, 1]')
+        return 1
+    else:
+        fib_sequence = [0,1]
+        for i in range(2,length):
+            next=fib_sequence[i-1] + fib_sequence[i-2]
+            fib_sequence.append(next)
+        print(f"[{', '.join(str(num) for num in fib_sequence)}]")
+        return fib_sequence
+print_fibonacci(10)
